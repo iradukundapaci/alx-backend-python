1e09e791e547a5991ffd24da0463382786bf9e12
iradukundapaci <paifique@gmail.com>
2024-03-15 17:56:21 +0200

    adding task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module to handle float nubers
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Function to turn float number to integer
+
+    args:
+        n: float to turn
+
+    return: integer
+    """
+    return int(n)

28ab5cc9a886737da92c1a44a8f31601052281dc
iradukundapaci <paifique@gmail.com>
2024-03-15 17:47:49 +0200

    adding task one

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+module for the addition function
+"""
+
+def add(a: float, b: float) -> float:
+    """
+    function that add two floats and return a float answer
+
+    args:
+        a: float operand
+        b: float operand
+    return: sum of a and b
+    """
+    return a + b

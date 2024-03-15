b15cd4f11bddf60e8ba763f59807bc5fcaf2ff78
iradukundapaci <paifique@gmail.com>
2024-03-15 17:59:09 +0200

    adding task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module to handle floats
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Function to convert float to string
+
+    args:
+        n: float to turn
+
+    return: string replesentation of float
+    """
+    return str(n)

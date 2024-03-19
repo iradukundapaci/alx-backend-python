9f0c0cc8ebb42e1d6e478f91c2e5b6d910b8ffcf
iradukundapaci <paifique@gmail.com>
2024-03-19 22:12:49 +0200

    editing types

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,11 +3,11 @@
 PYTHON ASYNC MODULE
 """
 import asyncio
-from typing import AsyncGenerator
+from typing import Generator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Function that creates async generator
 
@@ -15,4 +15,4 @@ async def async_generator() -> AsyncGenerator[float, None]:
     """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10

456d2458d0feb619c65eecd52611b2bb08be31b1
iradukundapaci <paifique@gmail.com>
2024-04-07 12:39:38 +0200

    indeting function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -20,7 +20,9 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ("a", "b"), 2),
         ]
     )
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, output: int):
+    def test_access_nested_map(self, nested_map: Mapping,
+                               path: Sequence,
+                               output: int):
         """
         Function to test access_nested_map
 
